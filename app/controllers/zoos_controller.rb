2bb30702bec178e98e9d4f4e39b330a4144d5d44
arellaTV <jeremiah.arella@gmail.com>
2020-05-29 16:25:26 -0700

    Updates public facing zoos controller to only show

--- app/controllers/zoos_controller.rb
+++ app/controllers/zoos_controller.rb
@@ -7,42 +7,6 @@ class ZoosController < ApplicationController
     @zoo = Zoo.find(params[:id])
   end
 
-  def new
-    @zoo = Zoo.new
-  end
-
-  def edit
-    @zoo = Zoo.find(params[:id])
-  end
-
-  def create
-    @zoo = Zoo.new(zoo_params)
-    
-    if @zoo.save
-      redirect_to @zoo
-    else
-      render 'new'
-    end
-  end
-
-  def update
-    @zoo = Zoo.find(params[:id])
-   
-    if @zoo.update(zoo_params)
-      redirect_to @zoo
-    else
-      render 'edit'
-    end
-  end
-
-
-  def destroy
-    @zoo = Zoo.find(params[:id])
-    @zoo.destroy
-  
-    redirect_to zoos_path
-  end
-
   private
     def zoo_params
       params.require(:zoo).permit(:name, :description, :address);
