16f08fb470af2fe732270cdf74a26ab5159e2d51
arellaTV <jeremiah.arella@gmail.com>
2020-05-29 14:58:51 -0700

    Adds a destory method and rearranges order

--- app/controllers/zoos_controller.rb
+++ app/controllers/zoos_controller.rb
@@ -3,6 +3,18 @@ class ZoosController < ApplicationController
     @zoos = Zoo.all
   end
 
+  def show
+    @zoo = Zoo.find(params[:id])
+  end
+
+  def new
+    @zoo = Zoo.new
+  end
+
+  def edit
+    @zoo = Zoo.find(params[:id])
+  end
+
   def create
     @zoo = Zoo.new(zoo_params)
     
@@ -13,18 +25,6 @@ class ZoosController < ApplicationController
     end
   end
 
-  def edit
-    @zoo = Zoo.find(params[:id])
-  end
-
-  def new
-    @zoo = Zoo.new
-  end
-
-  def show
-    @zoo = Zoo.find(params[:id])
-  end
-
   def update
     @zoo = Zoo.find(params[:id])
    
@@ -33,6 +33,14 @@ class ZoosController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+
+  def destroy
+    @zoo = Zoo.find(params[:id])
+    @zoo.destroy
+  
+    redirect_to zoos_path
   end
 
   private
