8808dedc5a2ff65af00406552f73bf7d7fb323e6
arellaTV <jeremiah.arella@gmail.com>
2020-05-29 15:10:25 -0700

    Associates Zoos with Animals

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,5 @@
 class Animal < ApplicationRecord
   belongs_to :zoo
+  validates :name, presence: true, length: { maximum: 120 }
+  validates :description, presence: true, length: { maximum: 512 }
 end
--- app/models/zoo.rb
+++ app/models/zoo.rb
@@ -1,4 +1,5 @@
 class Zoo < ApplicationRecord
+  has_many :animals
   validates :name, presence: true, length: { maximum: 120 }
   validates :description, presence: true, length: { maximum: 512 }
   validates :address, length: { maximum: 120 }
