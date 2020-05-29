43fecb0182dfa676b3771db6f5fd1398a0a68f69
arellaTV <jeremiah.arella@gmail.com>
2020-05-29 15:17:20 -0700

    Adds a create method to the animals controller

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,2 +1,12 @@
 class AnimalsController < ApplicationController
+  def create
+    @zoo = Zoo.find(params[:zoo_id])
+    @animal = @zoo.animals.create(animal_params)
+    redirect_to zoo_path(@zoo)
+  end
+
+  private
+    def animal_params
+      params.require(:animal).permit(:name, :description)
+    end
 end
