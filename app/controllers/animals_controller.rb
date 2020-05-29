bf9f9a40b19c8693a68925562c27db32ae4baee0
arellaTV <jeremiah.arella@gmail.com>
2020-05-29 16:27:47 -0700

    Adds an Admin::Animals controller

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,19 +1,2 @@
 class AnimalsController < ApplicationController
-  def create
-    @zoo = Zoo.find(params[:zoo_id])
-    @animal = @zoo.animals.create(animal_params)
-    redirect_to edit_zoo_path(@zoo)
-  end
-
-  def destroy
-    @zoo = Zoo.find(params[:zoo_id])
-    @animal = @zoo.animals.find(params[:id])
-    @animal.destroy
-    redirect_to edit_zoo_path(@zoo)
-  end
-
-  private
-    def animal_params
-      params.require(:animal).permit(:name, :description)
-    end
 end
