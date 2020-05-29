19591d1274ec196cafe55d6a31be93a15917d3ad
arellaTV <jeremiah.arella@gmail.com>
2020-05-29 15:29:48 -0700

    Abstract animal form to its own partial

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -2,7 +2,7 @@ class AnimalsController < ApplicationController
   def create
     @zoo = Zoo.find(params[:zoo_id])
     @animal = @zoo.animals.create(animal_params)
-    redirect_to zoo_path(@zoo)
+    redirect_to edit_zoo_path(@zoo)
   end
 
   private
