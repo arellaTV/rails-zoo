514414cccefb53f43cd0b80251914bc14397b9ab
arellaTV <jeremiah.arella@gmail.com>
2020-05-29 16:22:09 -0700

    Deletes animals associated with zoo

--- app/models/zoo.rb
+++ app/models/zoo.rb
@@ -1,5 +1,5 @@
 class Zoo < ApplicationRecord
-  has_many :animals
+  has_many :animals, dependent: :destroy
   validates :name, presence: true, length: { maximum: 120 }
   validates :description, presence: true, length: { maximum: 512 }
   validates :address, length: { maximum: 120 }
