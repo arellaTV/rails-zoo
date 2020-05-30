d8e88d0c5539dae403f46a106c1033862c5b7a31
arellaTV <jeremiah.arella@gmail.com>
2020-05-29 17:15:39 -0700

    Checks if logged in before allowing admin visit

--- app/controllers/admin/zoos_controller.rb
+++ app/controllers/admin/zoos_controller.rb
@@ -1,10 +1,8 @@
 class Admin::ZoosController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @zoos = Zoo.all
-  end
-
-  def show
-    @zoo = Zoo.find(params[:id])
   end
 
   def new
