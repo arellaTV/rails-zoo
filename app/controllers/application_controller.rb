06e6a2797e2de0950a551eecc7218c8293ef1b71
arellaTV <jeremiah.arella@gmail.com>
2020-05-30 23:50:44 -0700

    Redirects to admin homepage after logging in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
-    stored_location_for(resource) || admin_zoos_path
+    stored_location_for(resource) || admin_path
   end  
 end
