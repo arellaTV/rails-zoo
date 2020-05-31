1e8bd979064e1235cdb1a8df57262bb977024ec1
arellaTV <jeremiah.arella@gmail.com>
2020-05-30 17:46:14 -0700

    Removes animal dependency on zoo

--- app/models/zoo.rb
+++ app/models/zoo.rb
@@ -1,6 +1,6 @@
 class Zoo < ApplicationRecord
   has_one_attached :cover_photo
-  has_many :animals, dependent: :destroy
+  has_many :animals
   validates :name, presence: true, length: { maximum: 120 }
   validates :description, presence: true, length: { maximum: 512 }
   validates :address, length: { maximum: 120 }
