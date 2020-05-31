3b6d3c90317d8819a25897101cb30f171bfabb63
arellaTV <jeremiah.arella@gmail.com>
2020-05-30 17:58:54 -0700

    Adds admin controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   get 'homepage/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # Frontend pages
   resources :zoos do
     resources :animals
   end
 
+  # Admin pages
   namespace 'admin' do
     resources :zoos do
       resources :animals
@@ -14,6 +16,8 @@ Rails.application.routes.draw do
 
     resources :animals
   end
+
+  get '/admin' => "admin#index"
 
   get '/user' => "admin/zoos#index", :as => :user_root
 
